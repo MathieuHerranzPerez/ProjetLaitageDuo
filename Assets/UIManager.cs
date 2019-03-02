3693c8d7006ffc090e0f52d37d0c9675f55041c4
Guillaume Bdt <bguillaume81@gmail.com>
2019-03-01 22:25:54 -0500

    map v2 + boutons de teleportations + debut boutons ressources

---
+++ Assets/UIManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField]
+    private TPManager tpManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TPtoSouth()
+    {
+        tpManager.TPtoSouth();
+    }
+
+    public void TPtoNorth()
+    {
+        tpManager.TPtoNorth();
+    }
+
+    public void TPtoEast()
+    {
+        tpManager.TPtoEast();
+    }
+
+    public void TPtoWest()
+    {
+        tpManager.TPtoWest();
+    }
+}
