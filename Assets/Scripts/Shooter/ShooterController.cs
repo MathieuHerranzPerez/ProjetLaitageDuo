dff3b54986b014fe806d43df4aa3e58270ba042d
Eleonore <el.pc.wald@gmail.com>
2019-03-01 22:24:28 -0500

    Movement and aiming (vaguely)

--- Assets/Scripts/Shooter/ShooterController.cs
+++ Assets/Scripts/Shooter/ShooterController.cs
@@ -1,21 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[RequireComponent(typeof(ShooterMotor))]
 public class ShooterController : MonoBehaviour
 {
 
-    private string MunitionColor;
+    [SerializeField] private float Speed = 5f;
+    [SerializeField] private float LookSensitivity = 3f;
+
+    private ShooterMotor motor;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        motor = GetComponent<ShooterMotor>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float _xMov = Input.GetAxisRaw("Horizontal");
+        float _zMov = Input.GetAxisRaw("Vertical");
+
+        Vector3 _motorHorizontal = transform.right * _xMov;
+        Vector3 _motorVetical = transform.forward * _zMov;
+
+        Vector3 _velocity = (_motorHorizontal + _motorVetical).normalized * Speed;
+
+        motor.Move(_velocity);
+
+        float _yRot = Input.GetAxisRaw("Mouse X");
+
+        Vector3 _rotation = new Vector3(0f, _yRot, 0f) * LookSensitivity;
+
+        motor.Rotate(_rotation);
+        float _xRot = Input.GetAxisRaw("Mouse Y");
+
+        Vector3 _cameraRotation = new Vector3(_xRot, 0f, 0f) * LookSensitivity;
+
+        motor.RotateCamera(_cameraRotation);
     }
 }
---
+++ Assets/Scripts/Shooter/ShooterMotor.cs
@@ -0,0 +1,71 @@
+﻿
+using System;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class ShooterMotor : MonoBehaviour
+{
+    [SerializeField]
+    private Camera cam;
+    private Vector3 velocity = Vector3.zero;
+    private Vector3 rotation = Vector3.zero;
+    private Vector3 cameraRotation = Vector3.zero;
+
+    private string MunitionColor;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Move(Vector3 _velocity)
+    {
+        velocity = _velocity;
+    }
+
+    void FixedUpdate()
+    {
+        PerformMovement();
+        PerformRotation();
+        PerformCameraRotation();
+    }
+
+    private void PerformCameraRotation()
+    {
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(rotation));
+        if (cam != null)
+        {
+            cam.transform.Rotate(-cameraRotation);
+        }
+    }
+
+    private void PerformRotation()
+    {
+        rb.MoveRotation(rb.rotation*Quaternion.Euler(rotation));
+    }
+
+    private void PerformMovement()
+    {
+        if(velocity != Vector3.zero)
+        {
+            rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+        }
+    }
+
+    public void Rotate(Vector3 _rotation)
+    {
+        rotation = _rotation;
+    }
+
+    public void RotateCamera(Vector3 _cameraRotation)
+    {
+        rotation = _cameraRotation;
+    }
+}
